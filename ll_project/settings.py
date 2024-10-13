957093e936cdb1f93fe99dd2066d60403e0b3054
sean <sryan123@hotmail.com>
2024-10-13 15:55:01 +0100

    Added custom 404 error page.

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q9em8r*dj2z@3imdf9(4a-5xn&r-lzssco_i=bwre%zxmw2-8k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [ 'localhost' ]
 
 
 # Application definition
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'll_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
